2171d1371caadba0862da09ea2becda22fb5563b
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-03 19:37:32 +0200

    another files creation

---
+++ augmentation.py
@@ -0,0 +1,9 @@
+class AugmentationHandler():
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"Klasa do obsługi augmentacji danych audio"
+    
+    def augment(self) -> None:
+        pass
---
+++ feature_extraction.py
@@ -0,0 +1,9 @@
+class FeatureExtractionHandler:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"Klasa do ekstrakcji cech z plików audio"
+    
+    def extract(self) -> None:
+        pass
