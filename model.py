ba8c737c20f8607bd8bb65fbe98c8077bdc914eb
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-05 11:20:10 +0200

    first model creation, no tested yet

--- model.py
+++ model.py
@@ -1,4 +1,19 @@
 import torch
 
 class Model(torch.nn.Module):
-    pass
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = torch.nn.Linear(64 * 13 * 13, 128)
+        self.fc2 = torch.nn.Linear(128, 2)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.pool(torch.nn.functional.relu(self.conv1(x)))
+        x = self.pool(torch.nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 13 * 13)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return self.sigmoid(x)
