49fb2434fc832b48520f0747ff9969a21dead998
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-08 17:19:33 +0200

    dataloader 2.0

--- dataloader.py
+++ dataloader.py
@@ -1,45 +1,24 @@
 import pandas as pd
-import numpy as np
+import librosa
 
-from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
 
-class DataLoaderHandler():
-    def __init__(self, dataframe: pd.DataFrame):
+class DataLoaderHandler(Dataset):
+    def __init__(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         self.dataframe = dataframe
 
     def __repr__(self) -> str:
-        return f"Klasa odpowiedzialna za wczytywanie danych z dataframe i ich podział na trzy zbiory"
+        return "Klasa do ładowania danych, podejście 2.0"
     
-    def get_dataframe(self) -> pd.DataFrame:
-        return self.dataframe
+    def __len__(self) -> int:
+        return len(self.dataframe)
     
-    def split_data(self):
-        dataframe = self.get_dataframe()
+    def __getitem__(self, idx: int):
+        audio_path = self.dataframe.iloc[idx]['file_path']
+        label = self.dataframe.iloc[idx]['healthy_status']
 
-        X = dataframe['MFCC']
-        X = np.asarray(X)
-        print(np.shape(X))
-        y = dataframe['healthy_status'].values
-        y = np.asarray(y)
+        # load audio file, extract MFCC features
+        y, sr = librosa.load(audio_path)
+        mfcc = librosa.feature.mfcc(y = y, sr = sr, n_mfcc = 13)
 
-        # split data into three sets: train, validation, test
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        
-        # if type(X_train) != np.ndarray:
-        #     X_train = np.array(X_train)
-
-        # if type(X_test) != np.ndarray:
-        #     X_test = np.array(X_test)
-
-        # if type(y_train) != np.ndarray:
-        #     y_train = np.array(y_train)
-
-        # if type(y_test) != np.ndarray:
-        #     y_test = np.array(y_test)
-
-        print(f"X_train type: {type(X_train)}")
-        print(f"X_test type: {type(X_test)}")
-        print(f"y_train type: {type(y_train)}")
-        print(f"y_test type: {type(y_test)}")
-
-        return X_train, X_test, y_train, y_test
+        return mfcc, label
