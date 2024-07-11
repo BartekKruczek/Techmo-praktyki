4f2f9bc289b228d794ebe5e6a866988a2c3a1285
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-11 13:29:40 +0200

    loading model

--- auto_features_extraction.py
+++ auto_features_extraction.py
@@ -1,6 +1,15 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+
 class AutoFeaturesExtraction:
     def __init__(self) -> None:
         pass
 
     def __repr__(self) -> str:
         return "Klasa do automatycznego ekstrahowania cech, YAMNet"
+
+    def load_model(self) -> tf.keras.Model:
+        yamnet_model_handle = 'https://tfhub.dev/google/yamnet/1'
+        yamnet_model = hub.load(yamnet_model_handle)
+
+        return yamnet_model
