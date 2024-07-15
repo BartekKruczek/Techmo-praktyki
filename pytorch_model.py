2244f1ae1fd0b6bbc858fab6074c9d6cf8e5e901
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-15 15:04:33 +0200

    minor changes

--- pytorch_model.py
+++ pytorch_model.py
@@ -4,15 +4,48 @@ import torch
 
 from transformers import Wav2Vec2Model, Wav2Vec2Processor
 from torchaudio.transforms import Resample
+from torch.utils.data import Dataset
 
-class PytorchModelHandler():
-    def __init__(self, dataframe: pd.DataFrame) -> None:
+class PytorchModelHandler(Dataset):
+    def __init__(self, dataframe: pd.DataFrame, classification: bool) -> None:
         self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
         self.processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-        self.dataframe = dataframe
+        self.dataframe = self.dataframe_cleaner()
+        self.classification = classification
 
     def __repr__(self) -> str:
         return "Klasa do obsługi modelu Pytorch"
+    
+    def __len__(self) -> int:
+        return len(self.dataframe)
+    
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, int]:
+        """
+        Zwraca parę w postaci tensora cech w postaci wektorowej oraz przypisaną etykietę.
+        """
+        pass
+
+    def dataframe_cleaner(self) -> pd.DataFrame:
+        df = self.dataframe
+
+        # drop rows with missing values
+        df = df.dropna()
+
+        if self.classification != "multi":
+            # iterate in healthy_status column, if pathological 1, if healthy 0
+            df['healthy_status'] = df['healthy_status'].apply(lambda x: 1 if x == 'pathological' else 0)
+        else:
+            class_mapping = {
+                'healthy': 0,
+                'SLI': 1,
+                'Dysarthria': 2,
+                'Cyste': 3,
+                'Laryngitis': 4,
+                'Phonasthenie': 5
+            }
+            df['healthy_status'] = df['healthy_status'].map(class_mapping)
+
+        return df
     
     def get_audio_paths(self) -> list[str]:
         return self.dataframe['file_path'].values.tolist()
