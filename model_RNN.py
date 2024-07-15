f2d9a434c6c94b0607ecc24889211adae5c5e0b6
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-15 19:39:24 +0200

    tworzenie sieci RNN

---
+++ model_RNN.py
@@ -0,0 +1,8 @@
+import torch
+
+class ModelRNNHandler():
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return "Klasa do obsługi modelu RNN z LSTM, biblioteka Pytorch"
