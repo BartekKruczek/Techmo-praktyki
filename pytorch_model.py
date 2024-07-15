f7caf1ee1f40201b0a34aaa79c0a2fe710d50a03
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-15 14:03:47 +0200

    ekstrakcja cech przy uzyciu Wav2Vec2

--- pytorch_model.py
+++ pytorch_model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import torchaudio
+import torch
 
 from transformers import Wav2Vec2Model, Wav2Vec2Processor
 
@@ -13,5 +15,21 @@ class PytorchModelHandler():
     def get_audio_path(self, dataframe: pd.DataFrame) -> list[str]:
         return dataframe['file_path'].values.tolist()
     
-    def get_feature(self) -> None:
-        pass
+    def get_audio(self) -> tuple(torch.Tensor, int): # type: ignore
+        audio_paths: list[str] = self.get_audio_path()
+
+        for elem in audio_paths:
+            try:
+                y, sr = torchaudio.load(elem)
+            except Exception as e:
+                continue
+
+            return y, sr
+        
+    def extract_features(self, audio: torch.Tensor, sr: int) -> torch.Tensor:
+        my_inputs = self.processor(audio.squeeze().numpy(), sampling_rate = sr, return_tensors = "pt", padding = True)
+
+        with torch.no_grad():
+            features = self.model(input_values = my_inputs.input_values).last_hidden_state
+            
+        return features.mean(dim = 1)
