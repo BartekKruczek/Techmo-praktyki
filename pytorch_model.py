7b72e7d14ac49f5f91edae50d3bbbcc7124ae9b6
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-15 13:46:29 +0200

    [CHECKPOINT] początek pracy z Wav2Vec2 modelem

--- pytorch_model.py
+++ pytorch_model.py
@@ -1,9 +1,17 @@
-from torchaudio.models import Wav2Vec2Model
+import pandas as pd
+
+from transformers import Wav2Vec2Model, Wav2Vec2Processor
 
 class PytorchModelHandler():
     def __init__(self) -> None:
-        # https://pytorch.org/audio/stable/generated/torchaudio.models.Wav2Vec2Model.html#torchaudio.models.Wav2Vec2Model
         self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
+        self.processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 
     def __repr__(self) -> str:
         return "Klasa do obsługi modelu Pytorch"
+    
+    def get_audio_path(self, dataframe: pd.DataFrame) -> list[str]:
+        return dataframe['file_path'].values.tolist()
+    
+    def get_feature(self) -> None:
+        pass
