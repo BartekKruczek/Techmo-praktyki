a29d9afb9b92dc1ccad9c91ef831e336219f7181
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-07-24 10:40:38 +0200

    dalsza praca nad ensemble lr

--- ensemble.py
+++ ensemble.py
@@ -6,3 +6,12 @@ class MyEnsemble(nn.Module):
         self.model1 = model1
         self.model2 = model2
         self.model3 = model3
+
+    def forward(self, x1, x2, x3):
+        x1 = self.model1(x1)
+        x2 = self.model2(x2)
+        x3 = self.model3(x3)
+
+        avg_out = torch.stack([x1, x2, x3]).mean(dim=0)
+
+        return avg_out
